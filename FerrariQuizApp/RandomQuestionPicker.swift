34920ef1e4bf0806f09e29057d43372902011d3e
Nathaniel Remy <nathanielremy1@gmail.com>
2017-03-08 22:03:26 -0400

    Before adding answer options

--- FerrariQuizApp/RandomQuestionPicker.swift
+++ FerrariQuizApp/RandomQuestionPicker.swift
@@ -7,5 +7,24 @@
 //
 
 import Foundation
+import UIKit
+import GameKit
 
+struct RandomQuestionPicker {
+    
+//Questions
+    var questions = [
+        ["Q": "In Ferrari F12 TDF, What does the 'TDF' stand for?", "A": "Tour de France"],
+        ["Q": "The Ferrari 458's exhaust system was designed after what car?", "A": "Ferrari F40"],
+        ["Q": "In Ferrari 458, what does the '458' stand for?", "A": "4.5 Litre V8"],
+        ["Q": "What hyper car did Ferrari release in 2016?", "A": "LaFerrari Aperta"],
+        ["Q": "What car was named after Ferrari's founder?", "A": "Ferrari Enzo"]
+    ]
+//print out random question
+    mutating func randomQuestion() -> String{
+        let index = GKRandomSource.sharedRandom().nextInt(upperBound: questions.count)
+//        questions = questions - [index]
+        return questions[index]["Q"]!
+    }
+}
 
--- FerrariQuizApp/ViewController.swift
+++ FerrariQuizApp/ViewController.swift
@@ -12,10 +12,30 @@ import GameKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answer1: UIButton!
+    @IBOutlet weak var answer2: UIButton!
+    @IBOutlet weak var answer3: UIButton!
+    @IBOutlet weak var answer4: UIButton!
+    @IBOutlet weak var proceedQuestions: UIButton!
+    
+    
+    
+    var randomQuestionPicker = RandomQuestionPicker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//rounded corners
+        answer1.layer.cornerRadius = 10
+        answer2.layer.cornerRadius = 10
+        answer3.layer.cornerRadius = 10
+        answer4.layer.cornerRadius = 10
+        proceedQuestions.layer.cornerRadius = 10
+        
+        
+        printQuestion()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +43,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+//When nextQuestion button is taped
+    @IBAction func nextQuestion() {
+        printQuestion()
+    }
+    
+//print new question to the questionLabel
+    func printQuestion() {
+        questionLabel.text = randomQuestionPicker.randomQuestion()
+    }
 }
 
