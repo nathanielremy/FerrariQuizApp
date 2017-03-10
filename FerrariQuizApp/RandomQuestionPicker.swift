cb2791c1381f829b965b1a7d766c58490cd1d941
Nathaniel Remy <nathanielremy1@gmail.com>
2017-03-10 09:03:43 -0400

    Answer verification... Correct answers turn green

--- FerrariQuizApp/RandomQuestionPicker.swift
+++ FerrariQuizApp/RandomQuestionPicker.swift
@@ -10,62 +10,57 @@ import Foundation
 import UIKit
 import GameKit
 
-class RandomQuestionPicker {
-    
-    var questions: [[String: String]] = [
-        ["Q": "In Ferrari F12 TDF, What does the 'TDF' stand for?", "A": "Tour de France"],
-        ["Q": "The Ferrari 458's exhaust system was designed after what car?", "A": "Ferrari F40"],
-        ["Q": "In Ferrari 458, what does the '458' stand for?", "A": "4.5 Litre V8"],
-        ["Q": "What hyper car did Ferrari release in 2016?", "A": "LaFerrari Aperta"],
-        ["Q": "What car was named after Ferrari's founder?", "A": "Ferrari Enzo"]
-    ]
+var questions: [[String: String]] = [
+    ["Q": "In Ferrari F12 TDF, What does the 'TDF' stand for?", "rightAnswer": "Tour de France", "false1": "Secret Ferrari abbreviation", "false2": "Total Demon Features", "false3": "Total Driving Force"],
+    ["Q": "The Ferrari 458's exhaust system was designed after what car?", "rightAnswer": "Ferrari F40", "false1": "Ferrari F430", "false2": "Ferrari F50", "false3": "Ferrari FF"],
+    ["Q": "In Ferrari 458, what does the '458' stand for?", "rightAnswer": "4.5 Litre V8", "false1": "458 horse power", "false2": "458 cars have been manufactured", "false3": "$458, 000"],
+    ["Q": "What hyper car did Ferrari release in 2016?", "rightAnswer": "LaFerrari Aperta", "false1": "LaFerrari", "false2": "F12 TDF", "false3": "GTC4 Lusso"],
+    ["Q": "What car was named after Ferrari's founder?", "rightAnswer": "Ferrari Enzo", "false1": "575 Maranello", "false2": "Scuderia", "false3": "SuperAmerica"]
+]
 
-    func randomQuestion(answer1: UIButton, answer2: UIButton, answer3: UIButton, answer4: UIButton) -> String {
+
+
+class RandomQuestionPicker {
+
+    
+    func randomQuestion(questionLabel: UILabel, answer1: UIButton, answer2: UIButton, answer3: UIButton, answer4: UIButton) {
+        
         let index = GKRandomSource.sharedRandom().nextInt(upperBound: questions.count)
-        let indexVal = questions[index]
         
-        if (indexVal == questions[0]) {
-            answer1.setTitle(indexVal["A"], for: .normal)
-            answer2.setTitle("Secret Ferrari abbreviation", for: .normal)
-            answer3.setTitle("Total Demon Features", for: .normal)
-            answer4.setTitle("Total Driving Force", for: .normal)
-            
-            
-        } else if (indexVal == questions[1]) {
-            answer2.setTitle(indexVal["A"], for: .normal)
-            answer1.setTitle("Ferrari F430", for: .normal)
-            answer3.setTitle("Ferrari F50", for: .normal)
-            answer4.setTitle("Ferrari Enzo", for: .normal)
-            
-            
-        } else if (indexVal == questions[2]) {
-            answer3.setTitle(indexVal["A"], for: .normal)
-            answer1.setTitle("458 HorsePower", for: .normal)
-            answer2.setTitle("458 cars have been manufactured", for: .normal)
-            answer4.setTitle("$458 000", for: .normal)
-            
-            
-        } else if (indexVal == questions[3]) {
-            answer4.setTitle(indexVal["A"], for: .normal)
-            answer1.setTitle("LaFerrari", for: .normal)
-            answer2.setTitle("F12 TDF", for: .normal)
-            answer3.setTitle("GTC4 Lusso", for: .normal)
-            
-            
-        } else if (indexVal == questions[4]) {
-            answer3.setTitle(indexVal["A"], for: .normal)
-            answer1.setTitle("575 Maranello", for: .normal)
-            answer2.setTitle("Scuderia", for: .normal)
-            answer4.setTitle("SuperAmerica", for: .normal)
-            
+        questionLabel.text = questions[index]["Q"]
         
-        } else {
-            print("Something went wrong")
+        let display = GKRandomSource.sharedRandom().nextInt(upperBound: 4)
+        
+        if (display == 0) {
+            answer1.setTitle(questions[index]["false1"], for: .normal)
+            answer2.setTitle(questions[index]["false2"], for: .normal)
+            answer3.setTitle(questions[index]["rightAnswer"], for: .normal)
+            answer4.setTitle(questions[index]["false3"], for: .normal)
+            
+        } else if (display == 1) {
+            answer2.setTitle(questions[index]["false3"], for: .normal)
+            answer1.setTitle(questions[index]["rightAnswer"], for: .normal)
+            answer3.setTitle(questions[index]["false2"], for: .normal)
+            answer4.setTitle(questions[index]["false1"], for: .normal)
+            
+        } else if (display == 2) {
+            answer3.setTitle(questions[index]["false1"], for: .normal)
+            answer1.setTitle(questions[index]["false3"], for: .normal)
+            answer2.setTitle(questions[index]["rightAnswer"], for: .normal)
+            answer4.setTitle(questions[index]["false2"], for: .normal)
+        
+        } else if (display == 3) {
+            answer3.setTitle(questions[index]["false2"], for: .normal)
+            answer1.setTitle(questions[index]["false1"], for: .normal)
+            answer2.setTitle(questions[index]["false3"], for: .normal)
+            answer4.setTitle(questions[index]["rightAnswer"], for: .normal)
+            
         }
+    
         
-        
-        
-        return indexVal["Q"]!
+//        questions.remove(at: index)
     }
+
 }
+
 
--- FerrariQuizApp/ViewController.swift
+++ FerrariQuizApp/ViewController.swift
@@ -17,10 +17,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var answer3: UIButton!
     @IBOutlet weak var answer4: UIButton!
     @IBOutlet weak var proceedQuestions: UIButton!
+    @IBOutlet weak var rightOrWrong: UILabel!
     
+    var correctAnswers: Int = 0
+    var answeredQuestions: Int = 0
+    let totalQuestions = 5
     
     
     var randomQuestionPicker = RandomQuestionPicker()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +38,9 @@ class ViewController: UIViewController {
         answer4.layer.cornerRadius = 10
         proceedQuestions.layer.cornerRadius = 10
         
+        proceedQuestions.isHidden = true
+        
+        rightOrWrong.isHidden = true
         
         printQuestion()
         
@@ -42,15 +50,71 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func verifyanswer(_ sender: UIButton) {
+        answeredQuestions += 1
+        proceedQuestions.isHidden = false
+        
+        if (sender == answer1 || sender == answer2 || sender == answer3 || sender == answer4) {
+            if (sender.currentTitle == questions[0]["rightAnswer"]) || (sender.currentTitle == questions[1]["rightAnswer"]) || (sender.currentTitle == questions[2]["rightAnswer"]) || (sender.currentTitle == questions[3]["rightAnswer"]) || (sender.currentTitle == questions[4]["rightAnswer"]) {
+                rightOrWrong.textColor = UIColor(colorLiteralRed: 0, green: 255/255, blue: 0, alpha: 1.0)
+                rightOrWrong.text = "Correct answer"
+                rightOrWrong.isHidden = false
+            
+            
+            } else {
+                
+                let answerButtons = [answer1, answer2, answer3, answer4]
+                
+                rightOrWrong.textColor = UIColor(colorLiteralRed: 255/255, green: 0, blue: 0, alpha: 1)
+                rightOrWrong.text = "Oops! Incorrect answer..."
+                rightOrWrong.isHidden = false
+                
+                // Buttons with the correct answer turn green
+                
+                for answer in answerButtons {
+                    
+                    if (answer?.currentTitle == questions[0]["rightAnswer"]) || (answer?.currentTitle == questions[1]["rightAnswer"]) || (answer?.currentTitle == questions[2]["rightAnswer"]) || (answer?.currentTitle == questions[3]["rightAnswer"]) || (answer?.currentTitle == questions[4]["rightAnswer"]) {
+                        
+                        answer?.backgroundColor = UIColor.green
+                    }
+                }
+            }
+        }
+    }
+    
+        
+        
+        
+        
 
-//When nextQuestion button is taped
+//When nextQuestion button is tapped
     @IBAction func nextQuestion() {
+        
+        answer1.backgroundColor = UIColor(colorLiteralRed: 255/255, green: 0, blue: 0, alpha: 0.6)
+        answer2.backgroundColor = UIColor(colorLiteralRed: 255/255, green: 0, blue: 0, alpha: 0.6)
+        answer3.backgroundColor = UIColor(colorLiteralRed: 255/255, green: 0, blue: 0, alpha: 0.6)
+        answer4.backgroundColor = UIColor(colorLiteralRed: 255/255, green: 0, blue: 0, alpha: 0.6)
+        
         printQuestion()
     }
     
 //print new question to the questionLabel
     func printQuestion() {
-        questionLabel.text = randomQuestionPicker.randomQuestion(answer1: answer1, answer2: answer2, answer3: answer3, answer4: answer4)
+    randomQuestionPicker.randomQuestion(questionLabel: questionLabel, answer1: answer1, answer2: answer2, answer3: answer3, answer4: answer4)
+        proceedQuestions.isHidden = true
+        rightOrWrong.isHidden = true
     }
 }
+
+
+
+
+
+
+
+
+
+
 
